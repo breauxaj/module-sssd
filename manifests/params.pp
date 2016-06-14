f5c189670dda4a40755fede7387d342a48fd805e
Adam Breaux <breauxaj@gmail.com>
2016-06-14 17:10:25 -0400

    Initial commit

---
+++ manifests/params.pp
@@ -0,0 +1,42 @@
+# Class: sssd::params
+#
+# This class set parameters used in this module
+#
+# Actions:
+#   - Defines numerous parameters used by other classes
+#
+class sssd::params {
+  $sssd_package_ensure = 'latest'
+
+  case $::operatingsystem {
+    'Amazon': {
+
+    }
+    'CentOS', 'OracleLinux', 'RedHat': {
+      case $::operatingsystemmajrelease {
+        '6': {
+
+        }
+        '7': {
+
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} ${::operatingsystemmajrelease} distribution.")
+        }
+      }
+    }
+    'Debian': {
+      case $::operatingsystemmajrelease {
+        '8': {
+
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} ${::operatingsystemmajrelease} distribution.")
+        }
+      }
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+    }
+  }
+}
